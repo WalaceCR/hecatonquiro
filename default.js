e31b636430379ac8757f75f8b8fad1d0c9225ece
WalaceCR <walace.dacunha@yahoo.com.br>
2019-01-23 17:44:29 -0200

    Evolving default.js

--- default.js
+++ default.js
@@ -6,4 +6,10 @@ function concatenar(a, b){
     return a + b;
 }
 
-module.exports = somar, concatenar;
+function retornarlista(lista, callbackadd){
+    for (let index = 0; index < lista.length; index++) {
+        callbackadd(lista[index]);
+    }
+}
+
+module.exports = somar, concatenar, retornarlista;
