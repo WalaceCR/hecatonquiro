7e757e2943543509b0f917e351551eea31050f0a
WalaceCR <walace.dacunha@yahoo.com.br>
2019-01-23 17:44:54 -0200

    Evolving default.test.js

--- default.test.js
+++ default.test.js
@@ -2,10 +2,20 @@ const somar = require('./default');
 
 const concatenar2 = require('./default');
 
+const retornolista = require('./default');
+
 test('adds 1 + 2 to equal 3', () => {
     expect(somar(1, 2)).toBe(3);
 });
 
 test('adds to words and return another one', () => {
-    expect(concatenar2('Walace', 'Luana')).toBe('WalaceELuana');
+    expect(concatenar2('Walace', 'Luana')).toBe('WalaceLuana');
+});
+
+//uso de mocks
+test("verify return from list", ()=>{
+    let mockCallback = jest.fn(x => 42 + x);
+    retornolista([0, 1, 2, 3, 1], mockCallback);
+    //expect(mockCallback.mock.calls[0][0]).toBe(0);
+    expect(mockCallback.mock.calls.length).toBe(0);
 });
