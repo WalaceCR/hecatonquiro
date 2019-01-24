294777f36dd600d7316da19aed9b2bb93b4b6f19
WalaceCR <walace.dacunha@yahoo.com.br>
2019-01-24 17:17:23 -0200

    Removed files

--- api.test.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-const API = 'https://jsonplaceholder.typicode.com/users';
-
-test('async API call with Jest', async () => {
-  const req = await axios.get(API);
-  const underTest = req.data[0];
-  expect(underTest.name).toBe('Leanne Graham');
-});
