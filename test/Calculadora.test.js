d0449841a43015dfca7bd0d8d7356ba7652f0130
WalaceCR <walace.dacunha@yahoo.com.br>
2019-01-24 17:32:22 -0200

    Adding Division and Multplication tests

--- test/Calculadora.test.js
+++ test/Calculadora.test.js
@@ -11,3 +11,11 @@ test('adds 1 + 2 to equal 3', () => {
 test('subtract 2 from 1 equal to 1', () => {
     expect(this.calcularClassteste.calcular('-',2,1)).toBe(1);
 });
+
+test('multiply 2 times 1 then the result will equal 2', () => {
+    expect(this.calcularClassteste.calcular('x',2,1)).toBe(2);
+});
+
+test('divide 2 by 2 then result will be equal to 1', () => {
+    expect(this.calcularClassteste.calcular('/',2,2)).toBe(1);
+});
