3a40f5f7f1dd805141a5ca35ce77b5dc262088f6
WalaceCR <walace.dacunha@yahoo.com.br>
2019-01-28 17:20:55 -0200

    Adding undefined test

--- test/Calculadora.test.js
+++ test/Calculadora.test.js
@@ -19,3 +19,7 @@ test('multiply 2 times 1 then the result will equal 2', () => {
 test('divide 2 by 2 then result will be equal to 1', () => {
     expect(this.calcularClassteste.calcular('/',2,2)).toBe(1);
 });
+
+test('Must return undefined', () => {
+    expect(this.calcularClassteste.calcular('10',2,2)).toBe('Undefined');
+});
